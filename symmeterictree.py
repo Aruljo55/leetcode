295554a6e557ca07ed20cdfdb4ca2b9abcf0246a
Aruljo55 <aruljothinanjappan12@gmail.com>
2025-01-11 21:22:35 +0530

    symmeteric tree

---
+++ symmeterictree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSymmetric(self, root):
+        # Helper function to check if two subtrees are mirrors of each other
+        def isMirror(t1, t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            return (t1.val == t2.val and
+                    isMirror(t1.left, t2.right) and
+                    isMirror(t1.right, t2.left))
+        
+        # If the tree is empty, it's symmetric
+        return isMirror(root.left, root.right) if root else True
