5013076ea0c0dec564284358a52dc52333ffc230
Aruljo55 <aruljothinanjappan12@gmail.com>
2026-01-07 09:09:52 +0530

    split-array-largest-sum

---
+++ sal.py
@@ -0,0 +1,26 @@
+class Solution:
+    def splitArray(self, nums, k):
+        left = max(nums)
+        right = sum(nums)
+
+        def can_split(max_sum):
+            count = 1
+            curr = 0
+            for n in nums:
+                if curr + n <= max_sum:
+                    curr += n
+                else:
+                    count += 1
+                    curr = n
+                    if count > k:
+                        return False
+            return True
+
+        while left < right:
+            mid = (left + right) // 2
+            if can_split(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
