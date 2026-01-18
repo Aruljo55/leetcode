a9df4c09a8b89ef0dd4c8092e679df08ad4684d3
Aruljo55 <aruljothinanjappan12@gmail.com>
2026-01-18 06:55:13 +0530

    arithmetic-slices-ii-subsequence

---
+++ as2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums):
+        n = len(nums)
+        dp = [{} for _ in range(n)]
+        result = 0
+
+        for i in range(n):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+
+                count = dp[j].get(diff, 0)
+                result += count
+
+                dp[i][diff] = dp[i].get(diff, 0) + count + 1
+
+        return result
