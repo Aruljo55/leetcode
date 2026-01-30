280ba36e1ddc7a3ba382aa747e59e1cafa5ab9ff
Aruljo55 <aruljothinanjappan12@gmail.com>
2026-01-30 11:41:12 +0530

    4 sum 2

---
+++ 4sum2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        from collections import defaultdict
+
+        count = defaultdict(int)
+        for a in nums1:
+            for b in nums2:
+                count[a + b] += 1
+
+        res = 0
+        for c in nums3:
+            for d in nums4:
+                res += count[-(c + d)]
+
+        return res
